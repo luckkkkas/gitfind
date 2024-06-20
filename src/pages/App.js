ca3c482ec74283a203da543315ab02e41f6ace56
luckkkkas <lucashbo321@gmail.com>
2024-06-19 21:05:46 -0300

    First Commit

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+
+import gitLogo from'../assets/25231.png'
+import {Container} from './styles';
+
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt='logo GitHub'/>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%
+    height: 100vh;
+
+    margin: 20px 0;
+
+    display: flex;
+    alignItems: flex-start;
+    justify-content: center;
+`
